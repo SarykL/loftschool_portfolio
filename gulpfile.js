74529c9da25887155bc7e5af6268f6df3066e4e6
SarykL <saryk.lav@gmail.com>
2016-01-16 21:46:01 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,132 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    spritesmith = require('gulp.spritesmith'),
+    rimraf = require('rimraf'),
+    pngquant = require('imagemin-pngquant');
+
+var path = {
+    build: {
+        html: 'dist/',
+        css: 'dist/css/',
+        img: 'dist/img/',
+        fonts: 'dist/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        style: 'src/style/main.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/**/*.html',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './dist'
+};
+
+
+// Build HTML
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) 
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html));
+});
+
+
+// Build Stylesheets
+
+gulp.task('style:build', function () {
+    gulp.src(path.src.style) 
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: ['src/style/'],
+            outputStyle: 'compressed',
+            sourceMap: true,
+            errLogToConsole: true
+        }))
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css));
+});
+
+
+// Create Spites
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('src/sprites/icon/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    imgPath: '../img/sprites/sprite.png',
+    cssName: '_sprite.scss',
+    paddingg: 5
+  }));
+  spriteData.img.pipe(gulp.dest('./dist/img/sprites/'));
+  spriteData.css.pipe(gulp.dest('./src/style/components/'));
+});
+
+
+// Image Optimisation
+
+gulp.task('image:build', function () {
+    gulp.src(path.src.img) 
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img));
+});
+
+
+
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+
+gulp.task('build', [
+    'html:build',
+    'style:build',
+    'fonts:build',
+    'image:build'
+]);
+
+
+// Clear App folder
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.style], function(event, cb) {
+        gulp.start('style:build');
+    });
+    watch([path.watch.img], function(event, cb) {
+        gulp.start('image:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('default', ['build', 'watch']);
